48b923fd0654178e082b9509e27682d3977c1ff9
JaviCCT <JaviCCT@users.noreply.github.com>
2021-04-07 17:42:19 +0200

    +prueba

--- src/ut5/reto1/veliz/cabello/Main.java
+++ src/ut5/reto1/veliz/cabello/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        system.out.println("Hola que tal");
+        System.out.println("Viva españa");
     }
     
 }
