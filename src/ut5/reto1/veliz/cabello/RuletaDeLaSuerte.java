f4e4a5806683cb64c74522aacacb86b827af484a
DrackMegam <desnartgafoin@gmail.com>
2021-04-12 14:09:28 +0200

    +tirarRuleta() & resolverPanel() & comprarVocal - sin soporte

--- src/ut5/reto1/veliz/cabello/RuletaDeLaSuerte.java
+++ src/ut5/reto1/veliz/cabello/RuletaDeLaSuerte.java
@@ -21,13 +21,14 @@ public class RuletaDeLaSuerte {
         Scanner usu = new Scanner(System.in);
         String respuestaUsuario;
         int numeroJugadores = 0;
-        boolean verificarJugadores;
+        // Para verificar el nº de jugadores y la acción del turno de estos.
+        boolean verificarAccion;
 
         // Inicio del juego.
         System.out.println("BIENVENIDA");
 
         do {
-            verificarJugadores = false;
+            verificarAccion = false;
             System.out.printf("Nº jugadores: ");
             respuestaUsuario = usu.nextLine();
 
@@ -37,9 +38,9 @@ public class RuletaDeLaSuerte {
                     || respuestaUsuario.equals("4")
                     || respuestaUsuario.equals("5")
                     || respuestaUsuario.equals("6")) {
-                verificarJugadores = true;
+                verificarAccion = true;
             }
-        } while (!verificarJugadores);
+        } while (!verificarAccion);
 
         // Una vez que conozco el nº jugadores, lo paso a int.
         numeroJugadores = Integer.parseInt(respuestaUsuario);
@@ -67,7 +68,6 @@ public class RuletaDeLaSuerte {
             Jugador jugador = new Jugador(nombresJugadores.poll(), 2000, 0);
             jugadores.add(jugador);
         }
-        
 
         // Aquí comienza el juego.
         // Turno del jugador que empieza, y para ver como avanzan los turnos..
@@ -79,20 +79,50 @@ public class RuletaDeLaSuerte {
         // TODO elegir paneles y crear dichos objetos.
         // TODO empezar turno de jugador y funcionalidad del juego.
         boolean juegoVivo = true;
+        respuestaUsuario = null;
+        int accionJugador;
         do {
+            verificarAccion = false;
             // Invoco el texto personalizado.
             Texto.comenzarTurno(jugadores.get(turnoJugador));
+            do {
+                System.out.printf("||> ");
+                respuestaUsuario = usu.nextLine();
+                // Verifico que haya escrito una opción correcta.
+                if (respuestaUsuario.equals("1") || respuestaUsuario.equals("2") || respuestaUsuario.equals("3")) {
+                    verificarAccion = true;
+                }
+            } while (!verificarAccion);
 
-            // TODO tirar de la ruleta y decir consonante.
-            /**
-             * Probabilidades de la ruleta. 0 - 7.5 --> Quiebra 7.6 - 15 -->
-             * Perder el turno 15.1 - 25 --> 0€ 25.1 - 50 --> 25€ 50.1 - 65 -->
-             * 50€ 65.1 - 75 --> 75€ 75.1 - 80 --> 100€ 80.1 - 83.75 --> 150€
-             * 83.76 - 87.5 --> 200 87.6 - 95 --> Vuelve a tirar 95.1 - 100 -->
-             * Vocal gratis, vuelve a tirar.
-             */
-            // TODO Resolver el panel y cagar los demáses boteses.
-            // TODO Comprar una vocal, comprobando bote y demáses.
+            // Registro la acción y la ejecuto.
+            accionJugador = Integer.parseInt(respuestaUsuario);
+            switch (accionJugador) {
+                case 1:
+                    System.out.println("AAAA");
+                    // TODO tirar de la ruleta y decir consonante.
+                    /**
+                     * Probabilidades de la ruleta. 0 - 7.5 --> Quiebra 7.6 - 15
+                     * --> Perder el turno 15.1 - 25 --> 0€ 25.1 - 50 --> 25€
+                     * 50.1 - 65 --> 50€ 65.1 - 75 --> 75€ 75.1 - 80 --> 100€
+                     * 80.1 - 83.75 --> 150€ 83.76 - 87.5 --> 200 87.6 - 95 -->
+                     * Vuelve a tirar 95.1 - 100 --> Vocal gratis, vuelve a
+                     * tirar.
+                     */
+                    tirarRuleta(jugadores.get(turnoJugador), usu);
+                    break;
+                case 2:
+                    System.out.println("BBBB");
+                    // TODO Resolver el panel y cagar los demáses boteses.
+                    // Aquí hay que meter partes del ahorcado.
+                    resolverPanel(jugadores.get(turnoJugador), usu);
+                    break;
+                case 3:
+                    System.out.println("CCCC");
+                    // TODO Comprar una vocal, comprobando bote y demáses.
+                    comprarVocal(jugadores.get(turnoJugador), usu);
+                    break;
+            }
+
             turnoJugador = avanzarTurno(turnoJugador, numeroJugadores - 1);
         } while (juegoVivo);
     }
@@ -113,23 +143,53 @@ public class RuletaDeLaSuerte {
         }
         return turno;
     }
-    
+
     /**
-     * Muestra el estado de la partida.
-     * USO: estadoPartida(jugadores, numeroJugadores);
+     * Muestra el estado de la partida. USO: estadoPartida(jugadores,
+     * numeroJugadores);
+     *
      * @param jugadores Jugadores actuales de la partida.
      * @param numeroJugadores Nº de jugadores actuales.
      */
-    public static void estadoPartida(List<Jugador> jugadores, int numeroJugadores){
+    public static void estadoPartida(List<Jugador> jugadores, int numeroJugadores) {
         System.out.println();
         System.out.println("Repasemos como va la partida. ");
         System.out.println();
-        for(int i =0;i<numeroJugadores;i++){
-            System.out.println("Jugador nº"+(i+1)+": "+jugadores.get(i).getNombreJugador());
-            System.out.println("--> Dinero: "+jugadores.get(i).getDinero());
-            System.out.println("--> Bote: "+jugadores.get(i).getBote());
+        for (int i = 0; i < numeroJugadores; i++) {
+            System.out.println("Jugador nº" + (i + 1) + ": " + jugadores.get(i).getNombreJugador());
+            System.out.println("--> Dinero: " + jugadores.get(i).getDinero());
+            System.out.println("--> Bote: " + jugadores.get(i).getBote());
             System.out.println();
         }
+    }
+
+    /**
+     * Método para tirar de la ruleta.
+     *
+     * @param jugador Jugador del turno actual.
+     * @param scanner Scanner para pulsar "enter" y tirar.
+     */
+    private static void tirarRuleta(Jugador jugador, Scanner scanner) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Intenta resolver el panel escribiéndolo por teclado.
+     * @param jugador Jugador del turno actual. 
+     * @param scanner Scanner para pulsar "enter" y tirar.
+     */
+    private static void resolverPanel(Jugador jugador, Scanner scanner) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Compra una vocal verificando si tiene dinero para ello.
+     * También comprueba si quedan vocales por comprar o no.
+     * @param jugador Jugador del turno actual. 
+     * @param scanner Scanner para pulsar "enter" y tirar.
+     */
+    private static void comprarVocal(Jugador jugador, Scanner scanner) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
