afab0f0c261a425e939dc3f1329f58e2004d1875
JaviCCT <JaviCCT@users.noreply.github.com>
2021-04-13 12:25:41 +0200

    TirarRultea

--- src/ut5/reto1/veliz/cabello/RuletaDeLaSuerte.java
+++ src/ut5/reto1/veliz/cabello/RuletaDeLaSuerte.java
@@ -8,6 +8,7 @@ package ut5.reto1.veliz.cabello;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.LinkedTransferQueue;
 
@@ -173,8 +174,60 @@ public class RuletaDeLaSuerte {
      * @param scanner Scanner para pulsar "enter" y tirar.
      */
     private static void tirarRuleta(Jugador jugador, Scanner scanner) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /**
+                     * Probabilidades de la ruleta. 0 - 7.5 --> Quiebra 7.6 - 15
+                     * --> Perder el turno 15.1 - 25 --> 0€ 25.1 - 50 --> 25€
+                     * 50.1 - 65 --> 50€ 65.1 - 75 --> 75€ 75.1 - 80 --> 100€
+                     * 80.1 - 83.75 --> 150€ 83.76 - 87.5 --> 200 87.6 - 95 -->
+                     * Vuelve a tirar 95.1 - 100 --> Vocal gratis, vuelve a
+                     * tirar.
+                     */       
+        double opcionRuleta =  Math.random()*100;
+        
+        if (opcionRuleta >=0 && opcionRuleta <=7.5){
+            System.out.println("Has caido en quiebra");
+            jugador.setBote(0);
+            System.out.println("Actualmente el bote es de: " + jugador.getBote());
+        }else if(opcionRuleta >=7.6 && opcionRuleta <=15){
+            System.out.println("Pierdes el turno");
+            
+        }else if(opcionRuleta >=15.1 && opcionRuleta <=25){
+            System.out.println("0€");
+        }else if(opcionRuleta >=25.1 && opcionRuleta <=50){
+            System.out.println("25€");
+            jugador.setBote(jugador.getBote() + 25);
+            System.out.println("Actualmente el bote es de: " + jugador.getBote());
+        }else if(opcionRuleta >=50.1 && opcionRuleta <=65){
+            System.out.println("50€");
+            jugador.setBote(jugador.getBote() + 50);
+            System.out.println("Actualmente el bote es de: " + jugador.getBote());     
+        }else if(opcionRuleta >=65.1 && opcionRuleta <=75){
+            System.out.println("75€");
+            jugador.setBote(jugador.getBote() + 75);
+            System.out.println("Actualmente el bote es de: " + jugador.getBote());
+        }else if(opcionRuleta >=75.1 && opcionRuleta <=80){
+            System.out.println("100€");
+            jugador.setBote(jugador.getBote() + 100);
+            System.out.println("Actualmente el bote es de: " + jugador.getBote());
+        }else if(opcionRuleta >=80.1 && opcionRuleta <=83.75){
+            System.out.println("150€");
+            jugador.setBote(jugador.getBote() + 150);
+            System.out.println("Actualmente el bote es de: " + jugador.getBote());
+        }else if(opcionRuleta >=83.76 && opcionRuleta <=87.5){
+            System.out.println("200€");
+            jugador.setBote(jugador.getBote() + 200);
+            System.out.println("Actualmente el bote es de: " + jugador.getBote());
+        }else if(opcionRuleta >=87.6 && opcionRuleta <=95){
+            System.out.println("Vuelves a tirar");
+            tirarRuleta();
+        }else if(opcionRuleta >=95.1 && opcionRuleta <=100){
+            System.out.println("Vocal Gratis");
+            
+        }
+        String consonante = scanner.nextLine();
+         
     }
+    turnoJugador = avanzarTurno(turnoJugador,numeroJugadores-1);
 
     /**
      * Intenta resolver el panel escribiéndolo por teclado.
